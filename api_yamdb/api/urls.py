2d00698fa81314db1869371a015f63db8ce21726
Dimalright <108340220+Dimalright@users.noreply.github.com>
2022-11-06 00:51:41 +0300

    Update urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,22 +1,28 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import Signup, Token
+from .views import Signup, Token, CategoryViewSet, GenreViewSet, CommentViewSet, TitleViewSet, ReviewViewSet, UserViewSet
 
 app_name = 'api'
 
 router_v1 = routers.DefaultRouter()
-router_v1.register('categories', CategoriesViewSet, basename='categories')
-router_v1.register('genres', GenresViewSet, basename='genres')
+router_v1.register('categories', CategoryViewSet, basename='categories')
+router_v1.register('genres', GenreViewSet, basename='genres')
 router_v1.register("titles", TitleViewSet, basename = 'titles')
 router_v1.register(r'users', UserViewSet, basename='users')
 router_v1.register(
-    r'^titles/(?P<titles_id>\d+)/comments', CommentViewSet, basename='comments'
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
 )
 
-
 urlpatterns = [
-    path('v1/auth/signup/', Signup.as_view(), name='signup'),
     path('v1/auth/token/', Token.as_view(), name='token'),
+    path('v1/auth/signup/', Signup.as_view(), name='signup'),
     path('v1/', include(router_v1.urls)),
 ]
