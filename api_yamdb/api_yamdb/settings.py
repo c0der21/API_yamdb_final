52fe42be13fdfa5a7dc975d920b18d87e2587362
Medbrat4669 <210nk@mail.ru>
2022-11-07 23:48:45 +0300

    repair pep

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -18,13 +18,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'rest_framework',
     'rest_framework_simplejwt',
-
     'django_filters',
     'djoser',
-
     'reviews',
     'api',
 
