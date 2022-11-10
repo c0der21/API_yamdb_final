dce4a974e935485b4affe044fdf300419d31e726
Dimalright <108340220+Dimalright@users.noreply.github.com>
2022-11-10 15:20:29 +0300

    Update permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,37 +1,34 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
-from reviews.models import ADMIN, MODERATOR
 
 class AdminPermission(BasePermission):
-    """Класс для разрешения доступа только администратору."""
-
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
             request.user.is_staff
             or request.user.is_superuser
-            or request.user.role == ADMIN
+            or request.user.role == 'admin'
         )
 
 
 class IsAdminUserOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return True
-        if request.user.is_authenticated:
-            return request.user.role == ADMIN
-        return False
-
-class AdminModeratorAuthorPermission(BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.method in SAFE_METHODS
-            or request.user.is_authenticated
-        )
+        return (request.method in SAFE_METHODS
+                or (request.user.is_authenticated and (
+                    request.user.role == 'admin' or request.user.is_superuser)))
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in SAFE_METHODS
-            or obj.author == request.user
-            or request.user.role == MODERATOR
-            or request.user.role == ADMIN
-        )
+        return (request.method in SAFE_METHODS
+                or request.user.role == 'admin' or request.user.is_superuser)
+
+
+class IsAuthorOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (request.method in SAFE_METHODS
+                or obj.author == request.user)
+
+
+class IsModeratorOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (request.method in SAFE_METHODS
+                or request.user.role == 'moderator')
+    
